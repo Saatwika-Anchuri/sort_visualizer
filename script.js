c0e65cb30c8535211a8513118c98570bd7455c93
Saatwika-Anchuri <anchurisaatwika47@gmail.com>
2026-04-28 00:19:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+if (algo === "bubble") {
+  return `
+    <div class="water-wave"></div>
+
+    <div class="seaweed s1"></div>
+    <div class="seaweed s2"></div>
+    <div class="seaweed s3"></div>
+
+    <div class="fish fish-right-shape r1">
+      <span class="fin-top"></span>
+      <span class="fin-side"></span>
+    </div>
+
+    <div class="fish fish-right-shape r2">
+      <span class="fin-top"></span>
+      <span class="fin-side"></span>
+    </div>
+
+    <div class="fish fish-left-shape l1">
+      <span class="fin-top"></span>
+      <span class="fin-side"></span>
+    </div>
+  `;
+}
